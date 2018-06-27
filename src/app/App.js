0fb6acec809808f3f9cca53b4ea82b9f513e86d7
Maken Hubbard <makhub26@outlook.com>
2018-06-26 21:34:08 -0500

    adding firebase get method

--- src/app/App.js
+++ src/app/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 
 import Listings from '../components/Listings/Listings';
-
 import Building from '../components/Building/Building';
-
 import ListingForm from '../components/ListingForm/ListingForm';
+import connection from '../firebaseRequests/connection';
+import listingRequests from '../firebaseRequests/listings';
 
 import './App.css';
 
 class App extends Component {
+  state = {
+    listings: [],
+  }
+  componentDidMount () {
+
+    connection();
+    listingRequests.getRequest()
+      .then((listings) => {
+        this.setState({ listings });
+      })
+      .catch((err) => {
+        console.error('error with listing GET', err);
+      });
+  }
+
   render () {
     return (
       <div className="App">
         <div className="col-sm-6">
-          <Listings />
+          <Listings listings={this.state.listings}/>
         </div>
         <div className="col-sm-6">
           <Building />
